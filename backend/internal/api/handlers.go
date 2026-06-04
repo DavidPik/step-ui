2c5d31e6ae7217a8db12323fedbcdb2e0f2a3c0e
DavidPik <123298047+DavidPik@users.noreply.github.com>
2026-06-04 23:21:36 +0200

    Update handlers.go
    Oprava názvu typu CertificatesResponse

--- backend/internal/api/handlers.go
+++ backend/internal/api/handlers.go
@@ -274,7 +274,7 @@ func DownloadCertificatePackage(database *db.Database) gin.HandlerFunc {
 
         client := step.NewClientFromSettings(settings)
 
-        resp := step.certificateResponse{
+        resp := step.CertificateResponse{
             Certificate: cert.CertificatePEM,
             PrivateKey:  cert.PrivateKeyPEM,
             CABundle:    cert.CAChainPEM,
