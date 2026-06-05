1899aeb2458d9acad69d1ba304cb2229f601ec2f
DavidPik <123298047+DavidPik@users.noreply.github.com>
2026-06-05 23:12:14 +0200

    Update utils.ts
    Aktualizované DownloadBase64File

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -43,20 +43,12 @@ export function downloadFile(data: string, filename: string, mimeType: string) {
   window.URL.revokeObjectURL(url)
 }
 
-export function downloadBase64File(base64Data: string, filename: string, mimeType: string) {
-  const byteCharacters = atob(base64Data)
-  const byteNumbers = new Array(byteCharacters.length)
-  for (let i = 0; i < byteCharacters.length; i++) {
-    byteNumbers[i] = byteCharacters.charCodeAt(i)
-  }
-  const byteArray = new Uint8Array(byteNumbers)
-  const blob = new Blob([byteArray], { type: mimeType })
-  const url = window.URL.createObjectURL(blob)
-  const link = document.createElement('a')
-  link.href = url
-  link.download = filename
-  document.body.appendChild(link)
-  link.click()
-  document.body.removeChild(link)
-  window.URL.revokeObjectURL(url)
+export function downloadBase64File(base64: string, filename: string, mimeType: string) {
+  const blob = new Blob([Uint8Array.from(atob(base64), c => c.charCodeAt(0))], { type: mimeType })
+  const url = URL.createObjectURL(blob)
+  const a = document.createElement('a')
+  a.href = url
+  a.download = filename
+  a.click()
+  URL.revokeObjectURL(url)
 }
