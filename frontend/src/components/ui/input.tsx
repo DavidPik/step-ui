cc4173eb5ab4090c01ef54dc38738400a2e0b2b3
DavidPik <123298047+DavidPik@users.noreply.github.com>
2026-06-06 18:53:09 +0200

    Create input.tsx
    Definice Inputu

---
+++ frontend/src/components/ui/input.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { cn } from "@/utils";
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export function Input({ className, ...props }: InputProps) {
+  return (
+    <input
+      className={cn(
+        "border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500",
+        className
+      )}
+      {...props}
+    />
+  );
+}
