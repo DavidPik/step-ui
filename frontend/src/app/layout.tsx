7634955f07e77d47ca38d928cdeb0dbd40c026bd
DavidPik <123298047+DavidPik@users.noreply.github.com>
2026-06-06 08:57:53 +0200

    Update layout.tsx
    Inicializace toasterů

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Navigation } from './navigation';
+import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
   title: 'step-ca Web UI',
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className="app-body">
         <Navigation />
         <main className="app-main">{children}</main>
+        <Toaster />
       </body>
     </html>
   );
